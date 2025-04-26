2414cb88d1dd78e148970178d41e053cc09dbdfc
Emrecan Turgul <turgul2606@gmail.com>
2025-04-26 16:42:39 +0300

    Created API Controller

--- Business/Repositories/UserOperationClaimRepository/Validation/FluentValidation/UserOperationClaimValidator.cs
+++ Business/Repositories/UserOperationClaimRepository/Validation/FluentValidation/UserOperationClaimValidator.cs
@@ -12,10 +12,8 @@ namespace Business.Repositories.UserOperationClaimRepository.Validation.FluentVa
     {
         public UserOperationClaimValidator()
         {
-            RuleFor(p => p.UserId).Must(isValidId).WithMessage("yetki asamasi icin kullanici secimi yapmalisin"); 
-           // RuleFor(p => p.UserId).GreaterThan(0).WithMessage("id 0 dan büyük olmalı"); 
-
-            RuleFor(p => p.OperationClaimId).NotEmpty().WithMessage("yetki asamasi icin yetki secimi yapmalisin");
+            RuleFor(p => p.UserId).Must(isValidId).WithMessage("yetki asamasi icin kullanici secimi yapmalisin");
+            RuleFor(p => p.OperationClaimId).Must(isValidId).WithMessage("yetki asamasi icin yetki secimi yapmalisin");
         }
         private bool isValidId(int id)
         {
@@ -27,5 +25,6 @@ namespace Business.Repositories.UserOperationClaimRepository.Validation.FluentVa
             {
                 return false;
             }
+        }
     }
 }
