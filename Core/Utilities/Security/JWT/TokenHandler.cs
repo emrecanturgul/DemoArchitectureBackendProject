4721059d8cef1c70bad3421463935c7787c3b20a
Emrecan Turgul <turgul2606@gmail.com>
2025-04-29 01:32:18 +0300

    Created Jwt token

---
+++ Core/Utilities/Security/JWT/TokenHandler.cs
@@ -0,0 +1,49 @@
+﻿using Entities.Concrete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.JWT
+{
+    public class TokenHandler : ITokenHandler
+    {    
+        IConfiguration configuration;
+        public TokenHandler(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public Token CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            Token token = new Token();
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Token:SecurityKey"])); 
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+            token.Expiration = DateTime.Now.AddMinutes(60); 
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+                issuer : configuration["Token:Issuer"], 
+                audience: configuration["Token:Audience"],
+                expires: token.Expiration,
+                notBefore: DateTime.Now,
+                signingCredentials: signingCredentials
+            );
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            token.AccessToken = jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
+            token.RefreshToken = CreateRefreshToken();
+            return token;   
+        }
+        public string CreateRefreshToken()
+        {
+            byte[] number = new byte[32]; 
+            using (RandomNumberGenerator random = RandomNumberGenerator.Create())
+            {
+                random.GetBytes(number);
+                return Convert.ToBase64String(number);
+            }
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,15 +6,51 @@ using Business.DependencyResolvers.Autofac;
 using DataAccess.Repositories.OperationClaimRepository;
 using DataAccess.Concrete.EntityFramework;
 using System.ComponentModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModel())); 
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModel()));
+
+//if we want to allow all requests, we can use this code
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyHeader().AllowAnyMethod()
+            .AllowAnyOrigin()); 
+
+});
+    
+
 
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyHeader().AllowAnyMethod()
+            .AllowAnyOrigin());
+
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true, 
+        ValidateLifetime = false, 
+        ValidateIssuerSigningKey = true, 
+        ValidIssuer = builder.Configuration["Token:Issuer"],
+        ValidAudience = builder.Configuration["Token:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
+        ClockSkew = TimeSpan.Zero 
+    };
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,7 +65,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowAllOrigins");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
